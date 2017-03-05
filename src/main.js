d448b904a5f585f51e8bbb6b2c92bffb424579ee
yintothayang <yintothayang@gmail.com>
2017-03-05 14:06:25 -0800

    stuff

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Button from 'syndicate-ui/src/main'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -13,3 +14,5 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+
